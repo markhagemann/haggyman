9d519e71801af88cc27c0887a38335bdfddf746f
Mark Hagemann <mhagemann09@gmail.com>
2019-04-30 21:25:55 +1000

    Update gatsby-cli and edit portfolio page and Company component

--- src/portfolio/components/Company.tsx
+++ src/portfolio/components/Company.tsx
@@ -4,7 +4,16 @@ interface CompanyProps {
   company: string;
   role?: string;
   years?: string;
+  noPosts?: boolean;
 }
+
+const containerClass: (props: LayoutProps) => string = props => {
+  const showSidebarOnMobile = !!props.showSidebarOnMobile ? true : false;
+  return classNames({
+    'container mx-auto max-w-650 px-5 my-8 md:mt-16 md:text-left md:pt-24 md:mx-0 md:px-8 md:ml-300': true,
+    'mt-32': !showSidebarOnMobile
+  });
+};
 
 const Company: React.SFC<CompanyProps> = (props: CompanyProps) => {
   return (
@@ -13,7 +22,7 @@ const Company: React.SFC<CompanyProps> = (props: CompanyProps) => {
       <h1 className="mb-2"> {props.company} </h1>
       {props.role && <h2 className="text-grey font-light text-xl mb-1"> {props.role} </h2>}
       {props.years && <p className="text-grey-dark font-exo"> {props.years}</p>}
-      <hr className="border-b border-gradient-r-blue-custom my-8" />
+      {!props.noPosts && <hr className="border-b border-gradient-r-blue-custom my-8" />}
     </div>
   );
 };
--- src/portfolio/index.tsx
+++ src/portfolio/index.tsx
@@ -46,7 +46,8 @@ const PortfolioIndex: React.SFC<PortfolioProps> = props => {
         {' '}
         I am passionate about anything I end up working on. Solving big problems is just as satisfying as making something pretty.
       </p>
-      <Company company="LT Network" role="Web, Content &amp; Custom Development" years="2016 - Present" />
+      <Company company="MSTS" role="Developer" years="2019 - Present" noPosts="true" />
+      <Company company="LT Network" role="Web, Content &amp; Custom Development" years="2016 - 2019" />
       {posts.map(({ node }, i: number) => {
         return node.frontmatter.company === 'LT Network' ? (
           <Preview
