450b1be26877ad14de66921bcbbd732ad02f6c03
Mark Hagemann <mhagemann09@gmail.com>
2022-09-23 22:23:51 +1000

    Rename folder to lowercase

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar/Sidebar';
+import Sidebar from './sidebar/Sidebar';
 
 interface LayoutProps {
   children?: React.ReactNode;
