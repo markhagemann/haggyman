a104b428e3f3580dbd20c567188d8932d4dbc582
Mark Hagemann <mhagemann09@gmail.com>
2022-09-23 22:40:00 +1000

    Fix netlify-honeypot for spam protection

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -19,8 +19,8 @@ const ContactForm: React.FC = () => {
       name="contact"
       method="post"
       action="/thanks"
+      netlify-honeypot="bot-field"
       data-netlify="true"
-      data-netlify-honeypot="bot-field"
       className="bg-blue-darker shadow-md rounded px-5 py-5 sm:px-8 sm:py-8 mb-4"
     >
       <input type="hidden" name="bot-field" />
